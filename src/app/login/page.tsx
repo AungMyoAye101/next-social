144af93c748080f655d37f2d9c4bd4a979286c6c
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-23 19:10:33 +0630

    update login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,35 @@
+"use client";
+
 import { Button, Input } from "@nextui-org/react";
-import React from "react";
+import Email from "next-auth/providers/email";
+import React, { useState } from "react";
 
 const page = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  console.log(user);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser((pre) => ({ ...pre, [name]: e.target.value }));
+    console.log(user);
+  };
+
+  const handleSubmit = () => {
+    if (user.email.length < 3) {
+      console.log("email is too short");
+      return;
+    }
+    console.log("successed");
+  };
+
   return (
     <section className="max-w-xl mx-auto py-10 px-4">
       {" "}
       <form
-        action=""
+        action={handleSubmit}
         className="flex flex-col gap-4 border border-gray-300 rounded-lg shadow-md p-4 items-center"
       >
         <h1 className="text-xl md:text-2xl lg:text-3xl font-bold font-serif">
@@ -18,6 +41,7 @@ const page = () => {
 
         <Input
           type="email"
+          name="email"
           label="Email"
           variant="flat"
           placeholder="Enter your email"
@@ -25,9 +49,11 @@ const page = () => {
           size="lg"
           radius="sm"
           className="border border-gray-400 rounded-md"
+          onChange={handleChange}
         />
         <Input
           type="password"
+          name="password"
           label="Password"
           variant="flat"
           placeholder="Enter your passwords"
@@ -35,8 +61,9 @@ const page = () => {
           size="lg"
           radius="sm"
           className="border border-gray-400 rounded-md"
+          onChange={handleChange}
         />
-        <Button color="primary" radius="sm" variant="shadow">
+        <Button color="primary" radius="sm" variant="shadow" type="submit">
           Login
         </Button>
       </form>
