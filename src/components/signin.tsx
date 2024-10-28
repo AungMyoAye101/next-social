0b7fd0a25fb3401d4a9c532ba59b367e360881e5
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-28 19:17:40 +0630

    add signup page

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const signin = () => {
-  return <div>signin</div>;
+const Signin = () => {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="name"
+        className="border border-gray-300 rounded-lg shadow-md px-2 py-1"
+      />
+      <input
+        type="email"
+        placeholder="email"
+        className="border border-gray-300 rounded-lg shadow-md px-2 py-1"
+      />
+      <input
+        type="password"
+        placeholder="password"
+        className="border border-gray-300 rounded-lg shadow-md px-2 py-1"
+      />
+    </form>
+  );
 };
 
-export default signin;
+export default Signin;
