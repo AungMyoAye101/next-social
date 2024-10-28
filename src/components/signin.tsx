0802607a131fb4d469b0655a53ddf2c21c184647
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-28 19:39:11 +0630

    add mongodb and mongoose

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Signin = () => {
   return (
-    <form>
+    <form className="max-w-80 flex flex-col gap-4 mx-auto mt-10 p-4 shadow-md rounded-md border border-gray-300">
+      <h1>Sign in Form</h1>
       <input
         type="text"
         placeholder="name"
@@ -18,6 +19,7 @@ const Signin = () => {
         placeholder="password"
         className="border border-gray-300 rounded-lg shadow-md px-2 py-1"
       />
+      <button className="bg-blue-400 rounded-md px-4 py-1">Sign Up</button>
     </form>
   );
 };
