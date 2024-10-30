e7357d94f798bd40e8d8960e7715e88ea6bbdf0f
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-30 22:14:12 +0630

    Manage session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,22 @@ export default async function Home() {
 
   console.log(session?.user);
   return (
-    <section className="min-h-screen flex justify-center items-center">
-      <div className="text-red-400 text-4xl ">{session?.user?.name}</div>
-      <div className="text-red-400 text-4xl ">{session?.user?.email}</div>
+    <section className="min-h-screen flex flex-col  gap-10 justify-center items-center">
+      {!session?.user && <div>Please Create an account.</div>}
+      {session?.user && (
+        <div>
+          <p>
+            {session.user.name} and {session.user.email}
+          </p>
+
+          <button>SignOut</button>
+        </div>
+      )}
       <div className="flex gap-4">
         <Link href={"/signup"} className="px-4 py-2 bg-blue-400 rounded-lg">
           SignUp
         </Link>
-        <Link href={"/Login"} className="px-4 py-2 bg-purple-400 rounded-lg">
+        <Link href={"/login"} className="px-4 py-2 bg-purple-400 rounded-lg">
           Login
         </Link>
       </div>
