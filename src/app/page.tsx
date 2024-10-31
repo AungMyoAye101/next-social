0f9e1733c29f86657bcb9d71d1bc32d31bf66d39
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-31 21:49:52 +0630

    add card components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,43 @@
 import { auth } from "@/auth";
+import Post from "@/components/Post";
 import SignOut from "@/components/SignOut";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 
-export default async function Home() {
-  const session = await auth();
-
-  console.log(session?.user);
+export default function Home() {
   return (
-    <section className="min-h-screen flex flex-col  gap-10 justify-center items-center">
-      {!session?.user && <div>Please Create an account.</div>}
-      {session?.user && (
-        <div>
-          <p>
-            {session.user.name} and {session.user.email}
-          </p>
+    <section className="flex w-full">
+      <div className="w-[25%]"></div>
 
-          <SignOut />
-        </div>
-      )}
-      <div className="flex gap-4">
-        <Link href={"/signup"} className="px-4 py-2 bg-blue-400 rounded-lg">
-          SignUp
-        </Link>
-        <Link href={"/login"} className="px-4 py-2 bg-purple-400 rounded-lg">
-          Login
-        </Link>
-      </div>
+      <main className=" w-full md:w-[45%] flex flex-col gap-4">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </main>
+      <div className="w-[25%]"></div>
     </section>
   );
+}
+{
+  /* <section className="min-h-screen flex flex-col  gap-10 justify-center items-center">
+{!session?.user && <div>Please Create an account.</div>}
+{session?.user && (
+  <div>
+    <p>
+      {session.user.name} and {session.user.email}
+    </p>
+
+    <SignOut />
+  </div>
+)}
+<div className="flex gap-4">
+  <Link href={"/signup"} className="px-4 py-2 bg-blue-400 rounded-lg">
+    SignUp
+  </Link>
+  <Link href={"/login"} className="px-4 py-2 bg-purple-400 rounded-lg">
+    Login
+  </Link>
+</div>
+</section> */
 }
