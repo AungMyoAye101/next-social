dfcdb8f1211a95db4d9f8d22b63ff6df80f63c9e
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-02 13:15:30 +0630

    add profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <section className="flex flex-col gap-4 ">
-      <div className="flex justify-center items-end">
-        <div className="size-20 rounded-full bg-purple-500"></div>
+    <section className="flex flex-col gap-4 space-y-2 ">
+      <div className="flex justify-center items-center bg-green-100 h-[30vh]">
+        <div className="size-40 rounded-full bg-purple-500"></div>
       </div>
-      <div>
-        <h1>Name</h1>
-        <h2>Email@gmail.com</h2>
-        <p>Lost and Life of the king.</p>
+      <div className="text-center ">
+        <h1 className="text-2xl font-bold font-serif ">Name</h1>
+        <h2 className="text-lg font-semibold font-serif text-gray-500">
+          Email@gmail.com
+        </h2>
+        <div className="flex items-center justify-center gap-1 text-base text-gray-400">
+          <span>69 Followers</span> .<span>69 Following</span>
+        </div>
       </div>
     </section>
   );
