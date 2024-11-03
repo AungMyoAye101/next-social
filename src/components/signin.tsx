3477d7ebafa7e8b2ee010576c280d7f4b695417e
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-03 15:51:39 +0630

    update signup form

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { Button, Input } from "@nextui-org/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { useFormStatus } from "react-dom";
+import { FaEnvelope, FaEye } from "react-icons/fa6";
 
 const Signin = () => {
   const router = useRouter();
+  const { pending } = useFormStatus();
 
   const submitHandler = async (data: FormData) => {
     const name = data.get("name") as string;
@@ -30,30 +35,49 @@ const Signin = () => {
   return (
     <form
       action={submitHandler}
-      className="max-w-80 flex flex-col gap-4 mx-auto mt-10 p-4 shadow-md rounded-md border border-gray-300"
+      className=" flex flex-col gap-4 mx-auto  px-6 py-8  shadow-md rounded-md border border-gray-300"
     >
-      <h1>Sign in Form</h1>
-      <input
-        type="text"
-        name="name"
-        placeholder="name"
-        className="border border-gray-300 rounded-lg shadow-md px-2 py-1"
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="email"
-        className="border border-gray-300 rounded-lg shadow-md px-2 py-1"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="password"
-        className="border border-gray-300 rounded-lg shadow-md px-2 py-1"
-      />
-      <button type="submit" className="bg-blue-400 rounded-md px-4 py-1">
+      <h1 className="text-2xl font-bold font-serif text-center mb-2">
         Sign Up
-      </button>
+      </h1>
+      <Input
+        type="text"
+        label="Name"
+        name="name"
+        radius="sm"
+        variant="faded"
+        placeholder="Enter your name"
+        labelPlacement="outside"
+      />
+      <Input
+        type="email"
+        label="Email"
+        name="email"
+        radius="sm"
+        variant="faded"
+        placeholder="you@example.com"
+        labelPlacement="outside"
+      />
+
+      <Input
+        type="password"
+        label="Password"
+        name="password"
+        radius="sm"
+        variant="faded"
+        placeholder="Enter your password"
+        labelPlacement="outside"
+      />
+
+      <Button color="primary" type="submit" radius="sm" isLoading={pending}>
+        Sign Up
+      </Button>
+      <Link
+        href={"/login"}
+        className="text-sm font-serif text-purple-500 font-semibold self-end"
+      >
+        Already have an account?
+      </Link>
     </form>
   );
 };
