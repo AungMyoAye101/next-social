967864dba25bfc4e70d4d0ec68bce7e7e1092b72
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-01 20:23:33 +0630

    feat: add custom scrollbar styles and update layout colors in sidebars

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 const page = () => {
   return (
-    <section className="flex-1  pt-20 px-4 pb-4">
+    <section className="flex-1  pt-20 px-4 pb-4 min-h-screen overflow-hidden overflow-y-scroll coustom-scrollbar">
       <div className="h-screen flex justify-center items-center bg-green-200">
         <h1 className="text-6xl font-bold font-serif text-center">Hello </h1>
         <p className="text-2xl ">You are on home page</p>
       </div>
       <div className="h-screen bg-red-200"></div>
-      <div className="h-screen bg-red-200"></div>
+      <div className="h-screen bg-yellow-200"></div>
+      <div className="h-screen bg-blue-200"></div>
     </section>
   );
 };
--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RightSideBar = () => {
   return (
-    <section className="w-64 sticky top-0 pt-20 px-4 pb-4 flex flex-col bg-purple-500 gap-4 h-screen overflow-hidden overflow-y-scroll">
+    <section className="coustom-scrollbar w-64 sticky top-0 pt-20 px-4 pb-4 flex flex-col bg-purple-500 gap-4 h-screen overflow-hidden overflow-y-scroll">
       <div className="flex gap-2 ">
         <div className="size-10 bg-red-400 "></div>
         <p className="font-semibold">Name</p>
