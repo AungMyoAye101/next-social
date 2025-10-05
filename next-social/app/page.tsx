2abfb952cd4f560d631e374ecac136af674d4907
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-10-05 19:56:58 +0630

    initial commit with pnpm and hero ui

---
+++ next-social/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@heroui/button";
+
+export default function Home() {
+  return (
+    <div>
+      hello
+      <Button color="warning" size="sm" radius="sm">Click me</Button>
+    </div>
+  );
+}
