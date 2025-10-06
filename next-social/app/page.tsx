ce7741ad8d80bdf71c308a70961edc0e9897cb7d
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-10-06 22:33:10 +0630

    FEAT: add signup and login pages.

--- next-social/app/page.tsx
+++ next-social/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@heroui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      hello
-      <Button color="warning" size="sm" radius="sm">Click me</Button>
+      <Link href={'/login'}>Login</Link>
+      <Link href={'/signup'}>Signup</Link>
     </div>
   );
 }
---
+++ next-social/app/signup/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from 'react'
+import { Button } from '@heroui/button';
+import { Input } from '@heroui/input'
+import { Form } from '@heroui/form'
+const page = () => {
+
+    const [submitted, setSubmitted] = React.useState(null);
+
+    const onSubmit = (e: any) => {
+        e.preventDefault();
+
+        const data = Object.fromEntries(new FormData(e.currentTarget));
+
+        setSubmitted(data);
+    };
+    return (
+        <Form className="w-full max-w-xs" onSubmit={onSubmit}>
+            <Input
+                isRequired
+                errorMessage="Please enter a valid email"
+                label="Email"
+                labelPlacement="outside"
+                name="email"
+                placeholder="Enter your email"
+                type="email"
+            />
+            <Button type="submit" variant="bordered">
+                Submit
+            </Button>
+            {submitted && (
+                <div className="text-small text-default-500">
+                    You submitted: <code>{JSON.stringify(submitted)}</code>
+                </div>
+            )}
+        </Form>
+
+
+
+    )
+}
+
+export default page
