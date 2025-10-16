78df30e180ab6d8f61db96a8b32d4bba126e93ef
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-10-16 21:32:21 +0630

    Feat : Add axios interceptors for refresh token logic.

--- next-social/config/axios.ts
+++ next-social/config/axios.ts
@@ -1,7 +1,49 @@
 
 import axios from "axios"
-export const base = process.env.NEXT_PUBLIC_BASE_URL
-export const axiosInstance = axios.create({
+export const base = process.env.NEXT_PUBLIC_BASE_URL;
+
+const axiosInstance = axios.create({
     baseURL: base,
+    headers: { "Content-Type": "application/json" },
     withCredentials: true
-})
+});
+
+//Check token and set bearer token to headers
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = "aaaaa";
+        config.headers.Authorization = `Bearer ${token}`
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        console.log(originalRequest, "Original request");
+        const status = error.response.status;
+        console.log(status);
+
+        if (status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const { data } = await axiosInstance.post('/auth/refresh')
+                const newAccessToken = data.token;
+
+                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+                return axiosInstance(originalRequest)
+            } catch (error) {
+                console.log(error)
+                return Promise.reject(error)
+            }
+
+
+        }
+    }
+)
+
+
+export default axiosInstance;
